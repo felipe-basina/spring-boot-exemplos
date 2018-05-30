db4a0169a9fc8977032c248f01660253ff508b9e
felipe-basina <felipe.basina@gmail.com>
2018-05-29 22:16:43 -0300

    Adicionado projeto.

---
+++ security-api/src/main/java/br/com/becommerce/security/api/securityapi/SecurityApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.becommerce.security.api.securityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApiApplication.class, args);
+	}
+}
