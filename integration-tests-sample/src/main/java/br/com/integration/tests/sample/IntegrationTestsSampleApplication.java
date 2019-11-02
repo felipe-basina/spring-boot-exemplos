47ba50e78fb2f350c0aad97f195a0ed22395a510
fnascimento <felipe.basina@gmail.com>
2019-11-02 18:02:46 -0300

    Adicionado projeto.

---
+++ integration-tests-sample/src/main/java/br/com/integration/tests/sample/IntegrationTestsSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.integration.tests.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestsSampleApplication.class, args);
+	}
+
+}
