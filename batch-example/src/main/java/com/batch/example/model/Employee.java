168591e1e4e5e20973ca6f4baee91bb459c82b6f
fnascimento <felipe.basina@gmail.com>
2020-07-13 16:17:13 -0300

    Corrigido tipo de estrategia para controle de chaves primaria.

--- batch-example/src/main/java/com/batch/example/model/Employee.java
+++ batch-example/src/main/java/com/batch/example/model/Employee.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 public class Employee implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
