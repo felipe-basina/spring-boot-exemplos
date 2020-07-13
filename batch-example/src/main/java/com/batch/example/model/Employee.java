ecd4a623626c782764833ae6f582d47c246bb9f2
fnascimento <felipe.basina@gmail.com>
2020-07-13 12:49:01 -0300

    Adicionado configuracoes iniciais + modelo.

---
+++ batch-example/src/main/java/com/batch/example/model/Employee.java
@@ -0,0 +1,29 @@
+package com.batch.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String department;
+
+    private Double salary;
+
+}
