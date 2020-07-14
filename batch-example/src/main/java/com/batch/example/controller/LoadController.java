a51f1409f4bebc34b0702b2e58ca2fdeba125249
Felipe Nascimento <felipe.nascimento@becommerce.com.br>
2020-07-14 10:11:49 -0300

    Adiiconado operacao para executar processo batch de leitura de dados em bd para escrita em arquivo csv.

--- batch-example/src/main/java/com/batch/example/controller/LoadController.java
+++ batch-example/src/main/java/com/batch/example/controller/LoadController.java
@@ -1,20 +1,24 @@
 package com.batch.example.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.batch.example.config.SpringBatchConfig;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -25,12 +29,35 @@ public class LoadController {
     private JobLauncher jobLauncher;
 
     @Autowired
-    private Job job;
+    @Qualifier(value = SpringBatchConfig.FROM_FILE_TO_DATABASE_QUALIFIER)
+    private Job fromFileToDatabaseJob;
+
+    @Autowired
+    @Qualifier(value = SpringBatchConfig.FROM_DATABASE_TO_FILE_QUALIFIER)
+    private Job fromDatabaseToFileJob;
 
     @GetMapping
-    public ResponseEntity load() {
+    @RequestMapping(value = "${application.controller.fromFile}")
+    public ResponseEntity loadFromFile() {
         try {
-            JobExecution jobExecution = this.jobLauncher.run(this.job, this.jobParameters());
+            JobExecution jobExecution = this.jobLauncher.run(this.fromFileToDatabaseJob, this.jobParameters());
+
+            log.info("Batch is running...");
+            while (jobExecution.isRunning()) {
+                log.info(".....");
+            }
+
+            return ResponseEntity.ok().body(jobExecution.getStatus());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    @RequestMapping(value = "${application.controller.fromDatabase}")
+    public ResponseEntity loadFromDatabase() {
+        try {
+            JobExecution jobExecution = this.jobLauncher.run(this.fromDatabaseToFileJob, this.jobParameters());
 
             log.info("Batch is running...");
             while (jobExecution.isRunning()) {
