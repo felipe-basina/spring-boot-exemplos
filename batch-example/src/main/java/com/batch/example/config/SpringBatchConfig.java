98fd17ddf47dd04484c1640ee76795ace79b1d1f
Felipe Nascimento <felipe.nascimento@becommerce.com.br>
2020-07-14 10:10:17 -0300

    Adicionado configuracoes de job para ler da base de dados e escrever em arquivo csv.

--- batch-example/src/main/java/com/batch/example/config/SpringBatchConfig.java
+++ batch-example/src/main/java/com/batch/example/config/SpringBatchConfig.java
@@ -1,6 +1,10 @@
 package com.batch.example.config;
 
-import com.batch.example.model.Employee;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,18 +14,27 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
+import com.batch.example.model.Employee;
+import com.batch.example.processor.Processor;
+import com.batch.example.processor.ProcessorEmployeeRetrieved;
 
 @Configuration
 @EnableBatchProcessing
@@ -44,6 +57,15 @@ public class SpringBatchConfig {
 
     @Value(value = "${application.db.password}")
     private String databasePassword;
+    
+    public static final String FROM_FILE_TO_DATABASE_QUALIFIER = "FROM_FILE_TO_DATABASE_QUALIFIER";
+    public static final String FROM_DATABASE_TO_FILE_QUALIFIER = "FROM_DATABASE_TO_FILE_QUALIFIER";
+    
+    public static final String ITEM_READER_FROM_FILE = "ITEM_READER_FROM_FILE";
+    public static final String ITEM_READER_FROM_DATABASE = "ITEM_READER_FROM_DATABASE";
+    
+    public static final String ITEM_WRITER_TO_DATABASE = "ITEM_WRITER_TO_DATABASE";
+    public static final String ITEM_WRITER_TO_FILE = "ITEM_WRITER_TO_FILE";
 
     @Bean
     public DataSource dataSource() {
@@ -58,11 +80,12 @@ public class SpringBatchConfig {
     }
 
     @Bean
-    public Job job(JobBuilderFactory jobBuilderFactory,
+    @Qualifier(value = FROM_FILE_TO_DATABASE_QUALIFIER)
+    public Job jobFromFileToDatabase(JobBuilderFactory jobBuilderFactory,
                    StepBuilderFactory stepBuilderFactory,
-                   ItemReader<Employee> itemReader,
-                   ItemProcessor<Employee, Employee> itemProcessor,
-                   ItemWriter<Employee> itemWriter) {
+                   @Qualifier(value = ITEM_READER_FROM_FILE) ItemReader<Employee> itemReader,
+                   @Qualifier(value = Processor.QUALIFIER) ItemProcessor<Employee, Employee> itemProcessor,
+                   @Qualifier(value = ITEM_WRITER_TO_DATABASE) ItemWriter<Employee> itemWriter) {
 
         Step step = stepBuilderFactory.get("ETL-file-load")
                 .<Employee, Employee>chunk(10)
@@ -71,13 +94,14 @@ public class SpringBatchConfig {
                 .writer(itemWriter)
                 .build();
 
-        return jobBuilderFactory.get("ETL-Load")
+        return jobBuilderFactory.get("ETL-Load-to-Database")
                 .incrementer(new RunIdIncrementer())
                 .start(step)
                 .build();
     }
 
     @Bean
+    @Qualifier(value = ITEM_READER_FROM_FILE)
     public FlatFileItemReader<Employee> fileItemReader(@Value(value = "${application.input.file}") Resource resource) {
         FlatFileItemReader<Employee> flatFileItemReader = new FlatFileItemReader<>();
         flatFileItemReader.setResource(resource);
@@ -105,5 +129,64 @@ public class SpringBatchConfig {
 
         return defaultLineMapper;
     }
+
+    @Bean
+    @Qualifier(value = FROM_DATABASE_TO_FILE_QUALIFIER)
+    public Job jobFromDatabaseToFile(JobBuilderFactory jobBuilderFactory,
+                   StepBuilderFactory stepBuilderFactory,
+                   @Qualifier(value = ITEM_READER_FROM_DATABASE) JdbcCursorItemReader<Employee> itemReader,
+                   @Qualifier(value = ProcessorEmployeeRetrieved.QUALIFIER) ItemProcessor<Employee, Employee> itemProcessor,
+                   @Qualifier(value = ITEM_WRITER_TO_FILE) FlatFileItemWriter<Employee> itemWriter) {
+
+        Step step = stepBuilderFactory.get("ETL-database-load")
+                .<Employee, Employee>chunk(10)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+
+        return jobBuilderFactory.get("ETL-Load-to-File")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+    
+	@Bean
+	@Qualifier(value = ITEM_READER_FROM_DATABASE)
+	public JdbcCursorItemReader<Employee> readerFromDatabase() {
+		JdbcCursorItemReader<Employee> reader = new JdbcCursorItemReader<>();
+		reader.setDataSource(this.dataSource());
+		reader.setSql("SELECT id, name, department, salary FROM employee");
+		reader.setRowMapper(new EmployeeRowMapper());
+		return reader;
+	}
+
+	public class EmployeeRowMapper implements RowMapper<Employee> {
+		@Override
+		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new Employee(rs.getLong("id"),
+					rs.getString("name"),
+					rs.getString("department"),
+					rs.getDouble("salary"), null);
+		}
+	}
+	
+	@Bean
+	@Qualifier(value = ITEM_WRITER_TO_FILE)
+	public FlatFileItemWriter<Employee> writerToFile(@Value(value = "${application.output.file}") FileSystemResource resource) {
+		FlatFileItemWriter<Employee> writer = new FlatFileItemWriter<Employee>();
+		writer.setResource(resource);
+		writer.setLineAggregator(new DelimitedLineAggregator<Employee>() {
+			{
+				setDelimiter(",");
+				setFieldExtractor(new BeanWrapperFieldExtractor<Employee>() {
+					{
+						setNames(new String[] { "id", "name", "department", "salary", "timestamp" });
+					}
+				});
+			}
+		});
+		return writer;
+	}
 
 }
